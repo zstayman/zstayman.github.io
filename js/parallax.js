da0a26f8650c53679876e3089ef2570a10e396fd
github_name <github_email>
2014-04-30 12:37:19 -0400

    parallax removed

--- js/parallax.js
+++ js/parallax.js
@@ -1,6 +1,6 @@
-$(window).on("scroll", function(){
-  $(".fourth").css("top", -1*$(window).scrollTop());
-  $(".third").css("top", -.75*$(window).scrollTop());
-  $(".second").css("top", -.5*$(window).scrollTop());
-  $(".first").css("top", -.25*$(window).scrollTop());
-});
+// $(window).on("scroll", function(){
+//   $(".fourth").css("top", -1*$(window).scrollTop());
+//   $(".third").css("top", -.75*$(window).scrollTop());
+//   $(".second").css("top", -.5*$(window).scrollTop());
+//   $(".first").css("top", -.25*$(window).scrollTop());
+// });
