535c2cef578fae10f57c39db89ac596f9b106f23
github_name <z.stayman@gmail.com>
2014-05-14 13:04:40 -0400

    scroll

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+// Scroll Animation
+// ======================================
+
+// This a function I made to grab the id of an achor tag and animate scrolling to it.
+
+function scrollToSection(id){
+  var section = $("div" + id);
+  $('body').animate({scrollTop: section.offset().top},'slow');
+};
+
+// This is how I find the relevant element to scroll on based on the menu item I click.
+
+$("#go .link").click(function() {
+  var linkID = $(this).attr('href');
+  debugger;
+  scrollToSection(linkID);
+});
